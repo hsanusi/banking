b4851cfda0cf667a7fad431053126a50867a7e88
Hammed Sanusi <ahmedsanusi@gmail.com>
2022-05-15 23:23:11 +0100

    second-commit-15052022

--- thirdparty/serializers.py
+++ thirdparty/serializers.py
@@ -5,6 +5,7 @@ class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = ('id',"title","surname","first_name","other_name","sex","date_of_birth","home_address","phone","status","date_joined")
+        
 
 class CustomerGroupSerializer(serializers.ModelSerializer):
     class Meta:
